3575c5845ce76673851299b4bf71085316424a3a
Caleb Bell <caleb@theceebee.com>
2023-01-02 19:11:52 +1100

    Complete Day 14, part 1.

--- src/day14.py
+++ src/day14.py
@@ -1,3 +1,5 @@
+from functools import lru_cache
+from typing import Optional
 
 
 class Cave:
@@ -16,7 +18,8 @@ class Cave:
 
     def __init__(self):
         self._rock: dict[int, dict[int, int]] = {}
-        self._sand_source: dict[int, dict[int, int]] = {}
+        self._sand_source: tuple[int, int] = (500, 0)
+        self._resting_sand: dict[int, dict[int, int]] = {}
 
     def __str__(self):
         result = ""
@@ -24,7 +27,12 @@ class Cave:
         max_ = self.max
         for y in range(max_[1] + 1):  # y-minimum is always 0.
             for x in range(min_[0], max_[0] + 1):
-                result += "#" if self.is_rock(x, y) else "+" if self.is_sand_source(x, y) else "."
+                result += (
+                    "#" if self.is_rock(x, y)
+                    else "o" if self.is_resting_sand(x, y)
+                    else "+" if self.is_sand_source(x, y)
+                    else "."
+                )
             if y != max_[1]:
                 result += "\n"
         return result
@@ -38,6 +46,20 @@ class Cave:
     def max(self) -> tuple[int, int]:
         max_x_values = [max(v) for v in self._rock.values()]
         return max(max_x_values), max(self._rock)
+
+    @property
+    def resting_sand_count(self) -> int:
+        result = 0
+        min_ = self.min
+        max_ = self.max
+        for y in range(max_[1] + 1):  # y-minimum is always 0.
+            for x in range(min_[0], max_[0] + 1):
+                result += self.is_resting_sand(x, y)
+        return result
+
+    @property
+    def sand_source(self) -> tuple[int, int]:
+        return self._sand_source
 
     def add_rock(self, x: int, y: int):
         self._rock.setdefault(y, dict())[x] = 1
@@ -61,28 +83,86 @@ class Cave:
         else:  # just in case, add a single coordinate of rock.
             self.add_rock(*end)
 
-    def add_sand_source(self, x: int, y:int):
-        self._sand_source.setdefault(y, dict())[x] = 1
+    def add_resting_sand(self, x: int, y: int):
+        self._resting_sand.setdefault(y, dict())[x] = 1
 
     def is_rock(self, x: int, y: int) -> bool:
         return y in self._rock and self._rock[y].get(x) == 1
 
-    def is_sand_source(self, x: int, y: int) -> bool:
-        return y in self._sand_source and self._sand_source[y].get(x) == 1
+    def is_resting_sand(self, x: int, y: int) -> bool:
+        return y in self._resting_sand and self._resting_sand[y].get(x) == 1
 
-    def get_sand_sources(self) -> list[tuple[int, int]]:
-        result: list[tuple[int, int]] = []
-        min_ = self.min
-        max_ = self.max
-        for y in range(max_[1] + 1):  # y-minimum is always 0.
-            for x in range(min_[0], max_[0] + 1):
-                if self.is_sand_source(x, y):
-                    result.append((x, y))
-        return result
+    def is_sand_source(self, x: int, y: int) -> bool:
+        return (x, y) == self._sand_source
+
+    # def get_sand_sources(self) -> list[tuple[int, int]]:
+    #     result: list[tuple[int, int]] = []
+    #     min_ = self.min
+    #     max_ = self.max
+    #     for y in range(max_[1] + 1):  # y-minimum is always 0.
+    #         for x in range(min_[0], max_[0] + 1):
+    #             if self.is_sand_source(x, y):
+    #                 result.append((x, y))
+    #     return result
+
+
+class CaveSim:
+
+    def __init__(self, cave: Cave):
+        self.cave = cave
+        self.active_sand: Optional[tuple[int, int]] = None
+
+    @lru_cache()
+    def is_in_cave(self, x: int, y: int) -> bool:
+        return y <= self.cave.max[1]
+
+    def tick(self):
+        if not self.active_sand:
+            self.active_sand = self.cave.sand_source
+            return
+
+        next_position = self.active_sand
+        for offset in [(0, 1), (-1, 0), (2, 0)]:
+            next_position = tuple(next_position[i] + offset[i] for i in range(2))
+
+            if not self.is_in_cave(*next_position):
+                break
+
+            if not (self.cave.is_rock(*next_position) or self.cave.is_resting_sand(*next_position)):
+                self.active_sand = next_position
+                return
+
+        # If we've made it this far then either the sand can't go anywhere else, or it's run out of the cave and cannot come to rest.
+        if self.is_in_cave(*next_position):
+            self.cave.add_resting_sand(*self.active_sand)
+
+        self.active_sand = None
+
+    def tick_until_next_spawn(self):
+        if not self.active_sand:
+            self.tick()
+
+        while self.active_sand is not None:
+            self.tick()
+
+    def tick_until_sand_cannot_rest(self):
+
+        if not self.active_sand:
+            self.tick()
+
+        count = self.cave.resting_sand_count
+
+        while True:
+            if self.active_sand is None:
+                if self.cave.resting_sand_count == count:
+                    break
+
+                count = self.cave.resting_sand_count
+
+            self.tick()
 
 
 if __name__ == "__main__":
-    cave = Cave.from_file("../input/day14sample.txt")
-    cave.add_sand_source(500, 0)
-    print(cave)
-    print(cave.get_sand_sources())
+    sim = CaveSim(Cave.from_file("../input/day14.txt"))
+    sim.tick_until_sand_cannot_rest()
+    print(sim.cave.resting_sand_count)
